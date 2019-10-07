ee801b19ab846923ed445cef8261f82b83b8e7ad
twesterhever <40121680+twesterhever@users.noreply.github.com>
2019-10-07 17:01:08 +0200

    Initial commit

---
+++ asn-lookup.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" asn-lookup [.py]
+
+This script provides a Unix socket for ASN lookups as a deamon service.
+Since the ASN database is expensive to read and to store in RAM, a
+centralised makes more sense than local insances in each Squid ASN helper.
+
+Socket path, permissions, et cetera can be specified using the constants
+below import section. """
+
+# Import needed packages
+import ipaddress
+import logging
+import logging.handlers
+import os.path
+import signal
+import socket
+import sys
+import threading
+import pyasn
+
+# Define constants...
+SOCKETPATH = "temp.sock"
+SOCKETPERMISSIONS = 0o1130
+ASNDBPATH = "/home/user/ipasn_20191002.dat"
+
+# Initialise logging (to "/dev/log" - or STDERR if unavailable - for level INFO by default)
+LOGIT = logging.getLogger('asn-lookup')
+LOGIT.setLevel(logging.DEBUG)
+
+if os.path.islink("/dev/log"):
+    HANDLER = logging.handlers.SysLogHandler(address="/dev/log")
+else:
+    HANDLER = logging.StreamHandler(stream=sys.stderr)
+
+LOGIT.addHandler(HANDLER)
+
+
+def load_asndb():
+    """ Function call: load_asndb() """
+
+    asndb = pyasn.pyasn(ASNDBPATH)
+    LOGIT.debug("ASN database set up with object '%s'", asndb)
+
+    return asndb
+
+
+class SockServ(object):
+    """ Genuine socket server class containing everything that is needed
+    for building, running and maintaining our local Unix socket. """
+
+    def __init__(self):
+        # Delete orphaned socket file, if any...
+        if os.path.exists(SOCKETPATH):
+            LOGIT.info("Deleting orphaned socket file...")
+            os.remove(SOCKETPATH)
+
+        LOGIT.debug("Settting up Unix socket @ '%s'  with permissions '%s' ...", SOCKETPATH, SOCKETPERMISSIONS)
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.sock.bind(SOCKETPATH)
+        self.sock.listen()
+
+        LOGIT.debug("Successfully created socket, good, now waiting for queries...")
+
+        while True:
+            client, addr = self.sock.accept()
+
+            # Set long timeout for socket, as Squid helpers are (hopefully) present
+            # over a long time and there is no need to kick them out periodically...
+            client.settimeout(864000)
+
+            # Each client gets its own tread assigned...
+            threading.Thread(target=self.handleclient, args=(client, addr),
+                             daemon=True).start()
+
+    def handleclient(self, client, addr):
+        """ Function call: handleclient([SockServ object, client, client's address])
+
+        This function receives IP addresses from a client, enumerates the corresponding
+        Autonomous System Number (ASN) and returns it, if any. """
+
+        LOGIT.debug("handleclient(): got connection from client '%s' with address '%s'", client, addr)
+
+        # A completely exploded IPv6 address is 39 bytes long, so setting
+        # buffer size to 40 bytes should be enough...
+        bufsize = 40
+
+        while True:
+            # Read data from client and clean it up afterwards (no trailing newline, decoded)
+            try:
+                data = client.recv(bufsize)
+                cleanstring = str(data.decode('utf-8').rstrip())
+            except socket.timeout:
+                # Handle timeouts while reading data...
+                LOGIT.info("Timeout on connection to client '%s' with address '%s' exceeded", client, addr)
+                client.close()
+                break
+            except (ConnectionResetError, BrokenPipeError):
+                LOGIT.debug("Connection was reset by client, bailing...")
+                client.close()
+                break
+
+            # Only check for valid IPv4/IPv6 address if data are not obviously invalid...
+            # Lower boundary: 6 bytes (e.g.: 1.2.3.4)
+            # Upper boundary: 39 bytes (e.g.: fe80:0000:0000:0000:0000:0000:0000:0001)
+            if data and cleanstring and len(cleanstring) > 6 and len(cleanstring) < 40:
+                try:
+                    ipobject = ipaddress.ip_address(cleanstring)
+                except ValueError:
+                    LOGIT.warning("Received unexpected data from client, discarding...")
+                    ipobject = ""
+            else:
+                ipobject = ""
+
+            # Discard invalid input from client (too long, too short, garbarge, no IPv4/IPv6 address, ...)
+            if not ipobject or not isinstance(ipobject, (ipaddress.IPv4Address, ipaddress.IPv6Address)):
+                LOGIT.warning("Discarding invalid input (%s bytes) from client...", len(data))
+                client.send("Invalid input received.\n".encode('utf-8'))
+
+                # Destroy buffers...
+                del data
+                del cleanstring
+                del ipobject
+
+                # ... and terminate connection to this client
+                LOGIT.info("Closing connection to misbehaving client '%s' (addr: '%s')", client, addr)
+                client.close()
+                break
+
+            # At this point, we are dealing with valid input.
+            # Look up ASN for given IP address...
+            try:
+                asn = ASNDB.lookup(cleanstring)[0]
+                if asn is None:
+                    asn = 0
+            except Exception as error:
+                LOGIT.warning("Failed to enumerate ASN for input '%s' (error: %s), returning zero...", cleanstring, error)
+                asn = 0
+            finally:
+                client.send(str(asn).encode('utf-8'))
+
+
+# Reload ASN DB on SIGHUP...
+signal.signal(signal.SIGHUP, load_asndb)
+
+# Initially load ASN database, so the script can handle queries straight away...
+ASNDB = load_asndb()
+
+# Call socket object for processing helper queries
+try:
+    SockServ()
+except KeyboardInterrupt:
+    LOGIT.info("Received KeyboardInterrupt, shutting down...")
+    sys.exit(0)
+
+# EOF
---
+++ asnbl-helper.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env -S python3 -u
+# -*- coding: utf-8 -*-
+
+""" asnbl-helper [.py]
+
+Squid helper script for enumerating the ASN (Autonomous System
+Number) of an IP address and querying it against a file- or
+DNS-based black- or whitelist. If a domain is given, it will be
+resolved to its IP addresses, which will then be checked against
+the specified black-/whitelist source. """
+
+# Import needed modules...
+import ipaddress
+import logging
+import logging.handlers
+import os.path
+import re
+import socket
+import sys
+import dns.resolver
+
+# Define constants... (ASNLIST may contain a path to an ASN black-/whitelist
+# file, or a FQDN for DNSBL usage)
+SOCKETPATH = "temp.sock"
+ASNLIST = sys.argv[1]
+
+# Initialise logging (to "/dev/log" - or STDERR if unavailable - for level INFO by default)
+LOGIT = logging.getLogger('squid-asnbl-helper')
+LOGIT.setLevel(logging.INFO)
+
+if os.path.islink("/dev/log"):
+    HANDLER = logging.handlers.SysLogHandler(address="/dev/log")
+else:
+    HANDLER = logging.StreamHandler(stream=sys.stderr)
+
+LOGIT.addHandler(HANDLER)
+
+
+def is_ipaddress(chkinput: str):
+    """ Function call: is_ipaddress(input)
+    Tests if input is an IP address. It returns True if it
+    is one (v4/v6 does not matter), and False if not."""
+
+    try:
+        ipaddress.ip_address(chkinput)
+        return True
+    except ValueError:
+        return False
+
+
+def is_valid_domain(chkdomain: str):
+    """ Function call: is_valid_domain(domain name)
+    Checks if given domain is valid, i.e. does not contain any
+    unspecified characters. It returns True if a domain was valid,
+    and False if not."""
+
+    # test if chkdomain is an IP address (should not happen here)
+    if is_ipaddress(chkdomain):
+        return False
+
+    # allowed characters
+    allowedchars = re.compile(r"(?!-)[a-z\d\-\_]{1,63}(?<!-)$", re.IGNORECASE)
+
+    if len(chkdomain) > 255 or "." not in chkdomain:
+        # do not allow domains which are very long or do not contain a dot
+        return False
+
+    if chkdomain[-1] == ".":
+        # strip trailing "." if present
+        chkdomain = chkdomain[:-1]
+
+    # check if sublabels are invalid (i.e. are empty, too long or contain
+    # invalid characters)
+    for sublabel in chkdomain.split("."):
+        if not sublabel or not allowedchars.match(sublabel):
+            # sublabel is invalid
+            return False
+
+    return True
+
+
+def resolve_addresses(domain: str):
+    """ Function call: resolve_address(domain)
+    This function takes a domain and enumerates all IPv4 and IPv6
+    records for it. They are returned as an array."""
+
+    # check if this is a valid domain...
+    if not is_valid_domain(domain):
+        return None
+
+    # enumerate IPv6 addresses...
+    ip6a = []
+    try:
+        for resolvedip in RESOLVER.query(domain, 'AAAA'):
+            ip6a.append(str(resolvedip))
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
+        pass
+
+    # enumerate IPv4 addresses...
+    ip4a = []
+    try:
+        for resolvedip in RESOLVER.query(domain, 'A'):
+            ip4a.append(str(resolvedip))
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer):
+        pass
+
+    # assemble all IP addresses and return them back
+    ips = ip6a + ip4a
+    return ips
+
+
+# Abort if no arguments are given...
+try:
+    if not sys.argv[1]:
+        print("BH")
+        sys.exit(127)
+except IndexError:
+    print("Usage: " + sys.argv[0] + " ASNBL1 ASNBL2 ASNBLn")
+    sys.exit(127)
+
+# Test if given arguments are paths or FQDNs....
+ASNBLDOMAIN = []
+ASNBLFILE = []
+
+for targument in sys.argv[1:]:
+    if is_valid_domain(targument):
+        ASNBLDOMAIN.append(targument.strip(".") + ".")
+    elif os.path.exists(targument):
+        ASNBLFILE.append(targument)
+    else:
+        print("BH")
+        sys.exit(127)
+
+# Set up resolver object
+RESOLVER = dns.resolver.Resolver()
+
+# Set timeout for resolving
+RESOLVER.timeout = 2
+
+# Establish connection to ASN lookup socket...
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+sock.connect(SOCKETPATH)
+
+# Read domains or IP addresses from STDIN in a while loop, resolve IP
+# addresses if necessary, and do ASN lookups against specified socket for
+# each IP address. Query all specified ASN black-/whitelists afterwards,
+# return OK if a ASN hits, or ERR if none was found.
+while True:
+    try:
+        QUERYSTRING = str(sys.stdin.readline().rstrip().split()[0])
+    except KeyboardInterrupt:
+        sys.exit(127)
+
+    # Abort if no STDIN input was received
+    if not QUERYSTRING:
+        break
+
+    # Check if input is an IP address or a valid domain, and return "BH"
+    # if none matches. In case of domains, resolve corresponding IP addresses
+    if is_ipaddress(QUERYSTRING):
+        IPS = [QUERYSTRING]
+    elif is_valid_domain(QUERYSTRING):
+        IPS = resolve_addresses(QUERYSTRING)
+    else:
+        print("BH")
+        continue
+
+    # Enumerate ASN for each IP address in $IPS...
+    ASNS = []
+    for singleip in IPS:
+        try:
+            sock.send(str(singleip).encode('utf-8'))
+            buf = sock.recv(64)
+
+            ASNS.append(int(buf))
+        except BrokenPipeError:
+            print("BH")
+            break
+
+    # TODO: Debugging code, remove afterwards...
+    print(ASNS)
+
+    # Query enumerated ASNs against specified black-/whitelist sources...
+    
+
+# EOF
