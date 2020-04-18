c449b4981520e842ac0594eb8bf2b32ca0aeb533
twesterhever <40121680+twesterhever@users.noreply.github.com>
2020-04-18 14:02:38 +0200

    asnbl-helper.py: remove sanity test for 1.1.1.1
    The Routeviews DB is currently returning garbage (AS7497, "Computer
    Network Information Center", CN) for this single IP address. Perhaps
    somebody is messing with RIR databases again or fails to BGP hijack
    1.1.1.1 (hint: CIDRs < /24 cannot be globally announced)? Anyway, this
    test is now removed.

--- asnbl-helper.py
+++ asnbl-helper.py
@@ -29,8 +29,7 @@ ASDIVERSITYTHRESHOLD = 5
 # threshold (useful for simple Fast Flux mitigation)?
 BLOCKDIVERSITYEXCEEDINGDST = False
 # List of IP/ASN tuples for socket testing purposes
-TESTDATA = [("1.1.1.1", 13335),
-            ("8.8.8.8", 15169),
+TESTDATA = [("8.8.8.8", 15169),
             ("194.95.245.140", 680),
             ("10.0.0.1", 0),
             ("127.0.0.1", 0),
